4252a3b61fb778fc3615c9aa9abce7361ed74b26
Patience Elliott <elliottpatience7@gmail.com>
2017-02-02 22:44:53 +0000

    Stop Names

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-$.getJSON('https://ajar-target.gomix.me/stops', function(response){
-    console.log(response)
-    
+$.getJSON('https://ajar-target.gomix.me/stops', function(response) {
+    // console.log(response)
+
     var keys = Object.keys(response)
-    
-    for (i = 0; i < keys.length; i++){
-        
-        
+
+    for (i = 0; i < keys.length; i++) {
+
+
         $(".container").append(
-            
-            "<div class= col-md-2>" +
-            
-            "<div class='well'>" + keys[i] + "<br>"+ "</div>"
-            
-            + "</div>"
-            
-            )
+
+            "<div class= col-md-3>" +
+
+            "<div class='well'>" + response[keys[i]].stop_name + "<br>" + "</div>"
+
+            +
+            "</div>"
+
+        )
     }
-}) 
+})
+
+$.getJSON('https://ajar-target.gomix.me/status', function(response){
+    console.log(response)
+})
